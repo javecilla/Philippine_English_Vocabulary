bbe422667d81f0da0d33edfae9bcb684dcdcf1f1
Jerome Avecilla <102340560+javecilla@users.noreply.github.com>
2024-12-06 01:33:36 -0800

    Update info.php

--- info.js
+++
@@ -1,3 +0,0 @@
-(function() {
-  console.info("For confidentiality of this project src code is not showing in this repository");
-})()
---
+++ info.php
@@ -0,0 +1,3 @@
+<?php
+
+echo "For confidentiality of this project src code is not showing in this repository";
